8290061e9f578e49e2f050e6492fa99ebf759da6
zeguilherme99 <zeguilherme008@hotmail.com>
2021-07-07 21:20:24 -0300

    Criada as funcões e classe

--- src/FuncoesMatematicas.java
+++ src/FuncoesMatematicas.java
@@ -1,4 +1,40 @@
 
 public class FuncoesMatematicas {
+	
+	public static void exibirMenu() {
+		System.out.println("PROGRAMA CALCULADORA");
+		System.out.println("Escolha sua opção!");
+		System.out.println("1 - Digitar valores");
+		System.out.println("2 - Realizar soma");
+		System.out.println("3 - Realizar subtração");
+		System.out.println("4 - Realizar divisão");
+		System.out.println("5 - Realizar multiplicação");
+		System.out.println("6 - Sair");
+	}
+	
+	public static void somar(double valor1, double valor2) {
+		System.out.println("\n\nRealizando a soma entre " + valor1 + " e " + valor2);
+		double soma = valor1 + valor2;
+		System.out.println("O resultado foi " + soma + "!\n\n");
+	}
+	
+	public static void subtrair(double valor1, double valor2) {
+		System.out.println("\n\nRealizando a subtração entre " + valor1 + " e " + valor2);
+		double subtracao = valor1 - valor2;
+		System.out.println("O resultado foi " + subtracao + "!\n\n");
+	}
+	
+	public static void dividir(double valor1, double valor2) {
+		System.out.println("\n\nRealizando a divisão entre " + valor1 + " e " + valor2);
+		double divisao = valor1 / valor2;
+		System.out.println("O resultado foi " + divisao + "!\n\n");
+	}
+	
+	public static void multiplicar(double valor1, double valor2) {
+		System.out.println("\n\nRealizando a multiplicação entre " + valor1 + " e " + valor2);
+		double multiplicacao = valor1 * valor2;
+		System.out.println("O resultado foi " + multiplicacao + "!\n\n");
+	}
+
 
 }
